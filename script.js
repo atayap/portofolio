7f7d4a0562a0fb726b217df5c2332acb8653e430
Atya-web678 <atayafikri624@gmail.com>
2025-01-04 20:04:43 +0700

    ganti

--- script.js
+++
@@ -1,7 +0,0 @@
-// Pastikan Animate.css sudah di-link di HTML
-const headerSection = document.getElementById('header');
-
-// Tambahkan kelas animasi setelah halaman selesai dimuat
-window.onload = () => {
-  headerSection.classList.add('animate__animated', 'animate__fadeIn');
-};
